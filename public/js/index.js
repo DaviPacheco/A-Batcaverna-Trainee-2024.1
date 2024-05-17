3509bef1fcf85169d67a1253a82e7eb4f8934e90
MiguelOliveira <migueldoliveira1@gmail.com>
2024-05-17 16:21:22 -0300

    prototipo do codigo em js

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+/* const tela = document.querySelector('tela');
+
+function abrirModal(idModal)
+{
+    document.getElementById(idModal).style.display = "flex";
+    tela.style.display = "block";
+}
+function fecharModal(idModal)
+{
+    document.getElementById(idModal).style.display = "none"
+}
+*/
