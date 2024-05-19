fccc6205dad887287ce21a08b1db0d09ed57e345
luishenrique45 <luishenriquelagofarias1@gmail.com>
2024-05-19 19:48:20 -0300

    modal

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,43 @@
-const tela = document.querySelector('tela');
 
-function abrirModal(idModal)
+
+function abrirmodal(iddModal)
 {
-    document.getElementById(idModal).style.display = "flex";
-    tela.style.display = "block";
+    document.getElementById(iddModal).style.display ="flex";
+    
 }
-function fecharModal(idModal)
+function fecharmodal(iddModal)
 {
-    document.getElementById(idModal).style.display = "none"
+    document.getElementById(iddModal).style.display ="none"
+} 
+
+function troca_list (num_pag){
+    listas = document.querySelectorAll("table")
+    buttons = document.querySelectorAll(".num_pag")
+    listas.forEach(lista => {
+        lista.style.display = "none"
+    });
+    listas [num_pag].style.display = "table"
+
+    buttons.forEach(button =>{
+        button.classList.remove("active")
+    });
+    buttons [num_pag].classList.add("active")
+}
+window.addEventListener('hashchange', function() {
+    var hashValue = window.location.hash.substr(1);
+    troca_list(Number(hashValue)-1)
+});
+if(window.location.hash.substr(1) == ""){
+troca_list(0);
+}
+else{
+    troca_list(Number(window.location.hash.substr(1))-1)
+}
+
+/* botoes pra paginação */
+function troca_relativa (valor){
+    pagina = Number(window.location.hash.substr(1));
+    if (pagina+valor >=1 && pagina+valor <= 2){  
+        location.href="#"+(pagina+valor)
+    }
 }
