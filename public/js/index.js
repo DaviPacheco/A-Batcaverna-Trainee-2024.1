b4e45affb86af104512007927ad42fb8a654525b
berigolon <berigolon@gmail.com>
2024-05-27 16:24:18 -0300

    organizando as branches

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-function abrirModal(idModal){
-    document.getElementById(idModal).style.display = "flex";
-}
-
-function fecharModal(idModal){
-    document.getElementById(idModal).style.display = "none";
-}
