be04ae2a3df33a57004df7c50eaab9e58c10a01d
berigolon <berigolon@gmail.com>
2024-06-07 01:31:24 -0300

    inicio do CRUD

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class AdminController
+{
+
+    public function index()
+    {
+        $posts = App::get('database')->selectAll('posts');
+        return view('site/index', compact('posts'));
+    }
+}
+
+?>
