e10761a8b7cbbfacb514857d3436c25ead9e9c97
Renan Couri <renancouri123@gmail.com>
2024-06-14 17:29:46 -0300

    Adição da função da função da pra fazer a busca por tírulo na QueryBuilder.

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -28,4 +28,24 @@ class QueryBuilder
             die($e->getMessage());
         }
     }
+    public function selectAllSearch($table, array $param){
+        $key=null;
+        $value=null;
+        foreach($param as $keyB=>$valueB){
+              $key=$keyB;
+              $value=$valueB;
+        }
+        $sql ="SELECT * FROM {$table} WHERE $key LIKE ?";
+        $titleValue='%'.$value.'%';
+        try{
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1,$titleValue);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS);
+        }
+        catch (Exception $e) {
+            die($e->getMessage());
+           }
+    }
 }
