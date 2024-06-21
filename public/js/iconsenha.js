f63e97634bc18f4ff98840f02b02147d6d4166ca
luishenrique45 <luishenriquelagofarias1@gmail.com>
2024-06-21 01:33:47 -0300

    ver senha js login

---
+++ public/js/iconsenha.js
@@ -0,0 +1,14 @@
+function iconsenha(){
+ var inputPass =document.getElementById('senha');
+var  btnPass=document.getElementById("btn");
+if(inputPass.type ==='password'){
+    inputPass.setAttribute('type','text');
+    btnPass.classList.replace(' bi-eye',' bi-eye-slash');
+} 
+else{
+    inputPass.setAttribute('type','password');
+    btnPass.classList.replace(' bi-eye-slash ','bi-eye');
+}
+
+
+}
