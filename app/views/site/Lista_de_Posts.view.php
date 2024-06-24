f815aca28b1df1106045718869a316fc4fbec926
berigolon <berigolon@gmail.com>
2024-06-24 18:39:13 -0300

    ajustando css paginação

--- app/views/site/Lista_de_Posts.view.php
+++ app/views/site/Lista_de_Posts.view.php
@@ -64,7 +64,8 @@
         endforeach;
             ?>
                 
-                <nav class="paginacao" style = "display: flex; justify-content: center; margin-top: 20px;" aria-label="Page navigation example">
+                <div class = "caixote" style = "margin-top: 877px; position: absolute; width: 739px;">
+                <nav class="paginacao" style = "display: flex; justify-content:center;" aria-label="Page navigation example">
   <ul class="paginacao" style = "list-style-type: none">
     <li class="page-item <?= $pagina_atual <= 1 ? "disabled" : "" ?>" style = "list-style-type: none">
       <a class="page-link" href="?pagina=<?= $pagina_atual - 1 ?>&search=<?php if(array_key_exists('search',$_GET)){ echo $_GET['search']; } ?>" aria-label="Previous">
@@ -89,6 +90,7 @@
     </li>
   </ul>
 </nav>
+                </div>
                 
              
                 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
