670fd4e92146075d20b6e8131f38c9dc1bfde17e
MiguelOliveira <migueldoliveira1@gmail.com>
2024-06-24 20:43:34 -0300

    implementação da sidebar

--- app/views/admin/dashboard.view.php
+++ app/views/admin/dashboard.view.php
@@ -15,8 +15,16 @@ use PHPMailer\Test\PHPMailer\LocalizationTest;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="/public/css/dashboard.css">
+    <!-- links da sidebar -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="../../../public/css/sidebar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
+    <?php require_once "sidebar.view.php"?>
     <div id="logobat">
         <img src="/public/assets/Logo_Batcaverna_Preto (2).svg">
     </div>
--- app/views/site/ListaDeUsuarios_ComModal.view.php
+++ app/views/site/ListaDeUsuarios_ComModal.view.php
@@ -11,19 +11,95 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/public/css/ModalEdicaoPost.css" />
     <script defer src="https://kit.fontawesome.com/10f37f7ffb.js" crossorigin="anonymous"></script>
+
+ <!-- links da sidebar -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="../../../public/css/sidebar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
+<nav id="Sidebar">
+       <!-- <div id="sair">
+            <a href="#">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+        </div>-->
+        <div id="space">
+
+        </div>
+        <div id="caixa_sidebar">
+            <ul id="items">
+                <li class="item"> <!-- o class é para aplicar o css em tudo que possui a mesma classe-->
+                    <a href="/">
+                        <span class="icon"><i class="bi bi-house-door"></i></span>
+                        <span class="descricao_do_item">
+                            Home
+                        </span>
+                    </a> 
+        
+                </li>
+                <li class="item"> <!-- o class é para aplicar o css em tudo que possui a mesma classe-->
+                    <a href="/users">
+                        <span class="icon"><i class="bi bi-people"></i></span>
+                        <span class="descricao_do_item">
+                            Users
+                        </span>
+                    </a> 
+        
+                </li>
+                <li class="item"> <!-- o class é para aplicar o css em tudo que possui a mesma classe-->
+                    <a href="/posts">
+                        <span class="icon"><i class="bi bi-postcard"></i></span>
+                        <span class="descricao_do_item">
+                            Posts
+                        </span>
+                    </a> 
+        
+                </li>
+                <li class="item"> <!-- o class é para aplicar o css em tudo que possui a mesma classe-->
+                    <a href="/dashboard">
+                        <span class="icon"><i class="bi bi-columns-gap"></i></span>
+                        <span class="descricao_do_item">
+                            Dashboard
+                        </span>
+                    </a> 
+        
+                </li>
+                <li class="item"> <!-- o class é para aplicar o css em tudo que possui a mesma classe-->
+                    <a href="/login">
+                        <span class="icon"><i class="bi bi-box-arrow-right"></i></span>
+                        <span class="descricao_do_item">
+                            Logout
+                        </span>
+                    </a> 
+        
+                </li>
+              </ul>
+        </div>
+      
+
+
+    </nav>
+    <script src="script.js"></script>
     <div class="back">
+    
     <div class="cont_user">
+    
         <p class="title_user">Lista de Usuários</p>
         <button class="bt_create" onclick="abrirmodal('divform')">Criar Usuário</button>
     </div>
+    
     <div class="cont_tab"> 
-
+    
         
 
         <table>
+        
             <thead>
+            
                 <tr class ="header_tab">
                       <th class="header_id">ID</th>
                         <th>Usuário</th>
@@ -164,7 +240,9 @@
         </form>
     </div>
     <?php endforeach; ?>
+    
 </body>
 <script defer src="../../../public/js/Paginacao.js"></script>
 <script src="../../../public/js/Modal_Luis.js"></script>
+
 </html>
