9b9e82305808f99024b7f3354890f34702e49711
berigolon <berigolon@gmail.com>
2024-06-24 19:36:23 -0300

    paginação lista de usuarios

--- app/views/site/ListaDeUsuarios_ComModal.view.php
+++ app/views/site/ListaDeUsuarios_ComModal.view.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/ModalEdicaoPost.css" />
     <script defer src="https://kit.fontawesome.com/10f37f7ffb.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -46,76 +47,36 @@
 <?php endforeach; ?> 
             </tbody>
         </table>
-        <table>
-            <thead>
-                <tr class ="header_tab">
-                    <th class="header_id">ID</th>
-                    <th>Usuário</th>
-                    <th>Email</th>
-                    <th class ="header_ac">Ação</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class ="body_tab">
-                    <td>6</td>
-                    <td>Davi</td>
-                    <td>pacheco@gotham.com</td>
-                    <td><button class="bt_vis" onclick="abrirmodal('divvs')" ><i class="fa-regular fa-eye"></i><span class="stx">Visualizar</span></button>
-                        <button class="bt_edit"onclick="abrirmodal('eddivform')" ><i class="fa-regular fa-pen-to-square"></i><span class="stx">Editar</span></button>
-                        <button class="bt_delete" onclick="abrirmodal('divdelete')" ><i class="fa-regular fa-trash-can"></i><span class="stx">Excluir</span></button>
-                    </td>
-                </tr>
-                <tr class ="body_tab">
-                    <td>7</td>
-                    <td>Super</td>
-                    <td>man@gotham.com</td>
-                    <td><button class="bt_vis" onclick="abrirmodal('divvs')" ><i class="fa-regular fa-eye"></i><span class="stx">Visualizar</span></button>
-                        <button class="bt_edit"onclick="abrirmodal('eddivform')" ><i class="fa-regular fa-pen-to-square"></i><span class="stx">Editar</span></button>
-                        <button class="bt_delete" onclick="abrirmodal('divdelete')" ><i class="fa-regular fa-trash-can"></i><span class="stx">Excluir</span></button>
-                    </td>
-                </tr>
-                <tr class ="body_tab">
-                    <td>8</td>
-                    <td>Mulher</td>
-                    <td>maravilha@gotham.com</td>
-                    <td><button class="bt_vis" onclick="abrirmodal('divvs')" ><i class="fa-regular fa-eye"></i><span class="stx">Visualizar</span></button>
-                        <button class="bt_edit"onclick="abrirmodal('eddivform')" ><i class="fa-regular fa-pen-to-square"></i><span class="stx">Editar</span></button>
-                        <button class="bt_delete" onclick="abrirmodal('divdelete')" ><i class="fa-regular fa-trash-can"></i><span class="stx">Excluir</span></button>
-                    </td>
-                </tr>
-                <tr class ="body_tab">
-                    <td>9</td>
-                    <td>Wayne</td>
-                    <td>not_batman@gotham.com</td>
-                    <td><button class="bt_vis" onclick="abrirmodal('divvs')" ><i class="fa-regular fa-eye"></i><span class="stx">Visualizar</span></button>
-                        <button class="bt_edit"onclick="abrirmodal('eddivform')" ><i class="fa-regular fa-pen-to-square"></i><span class="stx">Editar</span></button>
-                        <button class="bt_delete" onclick="abrirmodal('divdelete')" ><i class="fa-regular fa-trash-can"></i><span class="stx">Excluir</span></button>
-                    </td>
-                </tr>
-                <tr class ="body_tab">
-                    <td>10</td>
-                    <td>Lanterna</td>
-                    <td>verde-msm@gotham.com</td>
-                    <td><button class="bt_vis" onclick="abrirmodal('divvs')" ><i class="fa-regular fa-eye"></i><span class="stx">Visualizar</span></button>
-                        <button class="bt_edit"onclick="abrirmodal('eddivform')" ><i class="fa-regular fa-pen-to-square"></i><span class="stx">Editar</span></button>
-                        <button class="bt_delete" onclick="abrirmodal('divdelete')" ><i class="fa-regular fa-trash-can"></i><span class="stx">Excluir</span></button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="paginacao">
-           <button onclick="troca_relativa(-1)" class="seta_pag1"><i class="fa-solid fa-angles-left "></i></button>
-            <a href="#1" class="num_pag">1</a>
-            <a href="#2" class="num_pag">2</a>
-            <button onclick="troca_relativa(+1)"  class="seta_pag2"><i class="fa-solid fa-angles-right"></i></button>
-        </div>
-    </div>
-</div>
-<script defer src="../../../public/js/Paginacao.js"></script>
+        
+        <nav class="paginacao" style = "display: flex; justify-content: center; margin-top: 20px;" aria-label="Page navigation example">
+  <ul class="paginacao" style = "list-style-type: none">
+    <li class="page-item <?= $pagina_atual <= 1 ? "disabled" : "" ?>" style = "list-style-type: none">
+      <a class="page-link" href="?pagina=<?= $pagina_atual - 1 ?>" aria-label="Previous">
+        <span class = "text-dark" aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
 
-    <!--////////////////// MODAL ////////////////-->
+    <?php for($pagina = 1; $pagina <= $pagina_total; $pagina++): ?>
 
-    <!-- CRIAR USUARIO -->
+    <li class="page-item">
+      <a class="page-link <?= $pagina == $pagina_atual ? "active" : "" ?>" href="?pagina=<?= $pagina ?>">
+        <?= $pagina ?>
+      </a>
+  </li>
+
+    <?php endfor ?>
+
+    <li class="page-item <?= $pagina_atual >= $pagina_total ? "disabled" : "" ?>">
+      <a class="page-link" href="?pagina=<?= $pagina_atual + 1 ?>" aria-label="Next">
+        <span class = "text-dark" aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+<!--////////////////// MODAL ////////////////-->
+
+<!-- CRIAR USUARIO -->
 <div class="cont_modal" id="divform">
     <form class="divform" method="POST" action="users/create">
         <div class="nv"><h2 class="nc">CRIAR USUARIO</h2></div>
@@ -128,14 +89,14 @@
             <input class="in" type="password"placeholder="  digite sua senha" name="password" required>
         </div>
         <div class="af">       
-        <button type="submit"  class="f">SALVAR</button>
-        <button class="fe" type="button" onclick="fecharmodal('divform')">CANCELAR</button>
-         </div>
+            <button type="submit"  class="f">SALVAR</button>
+            <button class="fe" type="button" onclick="fecharmodal('divform')">CANCELAR</button>
+        </div>
     </form>
 </div>
-    
-    <!-- EDITAR -->
-     <?php foreach( $users as $user): ?>
+
+<!-- EDITAR -->
+<?php foreach( $users as $user): ?>
     <div class="cont_modal" id="eddivform<?=$user->id ?? '1'?>" >
         <form class="eddivform"   method="POST" action="users/edit">
             <div class="ednv"><h2 class="ednc">EDITAR USUARIO</h2></div>
@@ -151,48 +112,48 @@
             <div class="edaf">       
             <button type="submit" class="edf" >SALVAR</button>
             <button type="button" class="edfe" onclick="fecharmodal('eddivform<?=$user->id ?? '1'?>')">CANCELAR</button>
-            </div>
-     </form>   
+        </div>
+    </form>   
         </div>
     <!-- VISUALIZAR -->
-        <div class="cont_modal" id="divvs<?=$user->id ?? '1'?>">
+    <div class="cont_modal" id="divvs<?=$user->id ?? '1'?>">
         <div class="divvs">
             <div class="divti">
                 <h2 class="h2vs">VISUALIZAR USUÁRIO</h2>
             </div>
             <div class="cabeca_conteudo">
-            <div class="vsidu">
-                <p class="idu"> ID:</p>
+                <div class="vsidu">
+                    <p class="idu"> ID:</p>
                 <p class="idu2"> <?=$user->id ?? '1'?></p>
-
+                
             </div>
             <div class="divvn">
                 <p class="pvs"> Nome:</p> 
                 <p class="ppu"><?=$user->name ?></p>
             </div>
-
+            
             <div class="divve">
                 <p class="pvs">Email:</p>
                 <p class="ppu2"><?=$user->email ?></p>
             </div>
-            </div>
+        </div>
             <div class="cabeca_butao">
-            <button type="button" class="butvs" onclick="fecharmodal('divvs<?=$user->id ?? '1'?>')" >FECHAR</button>
-            </div>
+                <button type="button" class="butvs" onclick="fecharmodal('divvs<?=$user->id ?? '1'?>')" >FECHAR</button>
             </div>
         </div>
     </div>
-    <!-- EXCLUIR -->
-    <div class="cont_modal" id="divdelete<?=$user->id ?? '1'?>">
-        <form class="divdelete" method="POST" action="users/delete">
+</div>
+<!-- EXCLUIR -->
+<div class="cont_modal" id="divdelete<?=$user->id ?? '1'?>">
+    <form class="divdelete" method="POST" action="users/delete">
             <div class="texti">
-               <h2 class="exu">EXCLUIR USUÁRIO</h2>
+                <h2 class="exu">EXCLUIR USUÁRIO</h2>
             </div>
             <div class="divaviso">
-              <p class="deletext">
-               Atenção, após essa ação for concluida, não será possivel desfazê-la!<br/>
-               Tem certeza que deseja excluir esse Usuário?
-              </p>
+                <p class="deletext">
+                    Atenção, após essa ação for concluida, não será possivel desfazê-la!<br/>
+                    Tem certeza que deseja excluir esse Usuário?
+                </p>
             </div>
             <div class="bbpos"> 
               <input type="hidden" value="<?=$user->id ?? '1'?>" name="id" >           
@@ -200,9 +161,10 @@
              <button class="butex" type="button" onclick="fecharmodal('divdelete<?=$user->id ?? '1'?>')">CANCELAR</button>
            </div>
             </div>
-     </form>
+        </form>
     </div>
-   <?php endforeach; ?>
+    <?php endforeach; ?>
 </body>
+<script defer src="../../../public/js/Paginacao.js"></script>
 <script src="../../../public/js/Modal_Luis.js"></script>
 </html>
