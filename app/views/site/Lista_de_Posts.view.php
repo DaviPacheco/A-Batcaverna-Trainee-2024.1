10300a10ca5e6852906aebc6370e17bed096e9d6
MiguelOliveira <migueldoliveira1@gmail.com>
2024-06-24 22:26:31 -0300

    footer na lista de posts

--- app/views/site/Lista_de_Posts.view.php
+++ app/views/site/Lista_de_Posts.view.php
@@ -33,6 +33,7 @@
   <?php
     require_once "navbar.html";
     ?>
+    
            <form id="barra" method="GET" action="">
                         <input id="Pesquisar" value="<?php if(array_key_exists('search',$_GET)){ echo $_GET['search']; } ?>" name="search" placeholder="Pesquisar" >
                         <button type="submit" id="lupa">
@@ -110,6 +111,7 @@
              
                 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
                 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  
                   <!-- Footer -->
     <?php
     require_once "footer.html"; 
